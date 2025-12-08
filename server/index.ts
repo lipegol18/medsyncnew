a06b0f8bbbbdb9f51aa103d5d5537bfca46cfa04
lipegol18 <felipecorreati@gmail.com>
2025-12-08 18:57:22 -0300

    upload 08122025

--- server/index.ts
+++ server/index.ts
@@ -355,7 +355,7 @@ app.use((req, res, next) => {
     res.send(html);
   });
 
-  // ALWAYS serve the app on port 5001
+  // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = 5001;
